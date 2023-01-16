c1fe27b252448255ff748746a94eea71c7bea086
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-01-16 17:37:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/essaifragment/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.essaifragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val fragmentManager = supportFragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // Au clique j'affiche le BlankFragment
+    fun onClickFirst(view: View) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.frameLayout, BlankFragment())
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    // Au clique j'affiche le SecondFragment
+    fun onClickSecond(view: View) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.frameLayout, SecondFragment())
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+}
