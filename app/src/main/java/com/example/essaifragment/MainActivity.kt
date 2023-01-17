235ffecc40bd1a7903847e6df98ed942a978a564
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-01-17 10:37:46 +0100

    Ajout de Log.i pour afficher le clique dans le terminal

--- app/src/main/java/com/example/essaifragment/MainActivity.kt
+++ app/src/main/java/com/example/essaifragment/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.essaifragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     // Je récupère le package fragmentManager
     private val fragmentManager = supportFragmentManager
+
+    private val TAG = "FirstFragment"
+    private val secondTAG = "SecondFragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     // Au clique j'affiche le BlankFragment
     fun onClickFirst(view: View) {
+        Log.i(TAG, "J'ai cliqué sur le bouton First !")
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.frameLayout, BlankFragment())
         fragmentTransaction.addToBackStack(null)
@@ -24,6 +29,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     // Au clique j'affiche le SecondFragment
     fun onClickSecond(view: View) {
+        Log.i(secondTAG, "J'ai cliqué sur le bouton Second !")
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.frameLayout, SecondFragment())
         fragmentTransaction.addToBackStack(null)
